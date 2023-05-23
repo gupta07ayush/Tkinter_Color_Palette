23d382abdeae5be69d0a1d3f33e29394691f4176
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-23 14:37:38 +0530

    make choose_color function

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ from tkinter import *
 # importing the choosecolor package
 from tkinter import colorchooser
 
+# function that will be invoked when the button will be clicked in the main window.
+
+
+def choose_color():
+
+    # variable to store hexadecimal code of color
+    color_code = colorchooser.askcolor(title="Choose your favorite color")
+    print(color_code)
+
+
 root = Tk()
-root.geometry(500x500)
+root.title('Tkinter Color Palette')
+root.geometry("500x500")
 
 # start the GUI loop
 root.mainloop()
