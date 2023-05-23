8eefeb9e9bbc3f54d5d3caa542dc731051764d37
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-23 14:34:01 +0530

    start the gui loop
    initialize the tkinter window

---
+++ main.py
@@ -0,0 +1,13 @@
+# Python Program to create color chooser dialog box
+
+# importing tkinter module
+from tkinter import *
+
+# importing the choosecolor package
+from tkinter import colorchooser
+
+root = Tk()
+root.geometry(500x500)
+
+# start the GUI loop
+root.mainloop()
